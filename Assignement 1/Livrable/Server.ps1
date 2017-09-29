86450adef30ad49d61c39efe9b5bb69bd6ccfc19
EtienneB <etienne.bernoux@edu.ece.fr>
2017-09-29 14:01:44 +0200

    commit power shell data

---
+++ Assignement 1/Livrable/Server.ps1
@@ -0,0 +1,20 @@
+﻿Remove-Item –path ./BESCOND-BERNOUX_code-java/bin/* –recurse
+#rm -rf ./BESCOND-BERNOUX_code-java/bin/*
+New-Item ./BESCOND-BERNOUX_code-java/bin -type directory
+
+
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\profileApp\SongHelper.java .\BESCOND-BERNOUX_code-java\src\profileApp\Song.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\profileApp\UserHelper.java .\BESCOND-BERNOUX_code-java\src\profileApp\User.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\profileApp\TopTenHelper.java .\BESCOND-BERNOUX_code-java\src\profileApp\TopTen.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\*
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\profileApp\*
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\client\FormatRequest.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\exceptions\UndefinedFonctionException.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\tools\*
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\server\*
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\client\*
+
+echo "Main-Class: server.ProfileServer
+" > ./BESCOND-BERNOUX_code-java/bin/Manifest.txt
+
+jar cfm server.jar ./BESCOND-BERNOUX_code-java/bin/Manifest.txt ./BESCOND-BERNOUX_code-java/bin/server/*.class
