1fee0dfad6fae9e29af519107430cc8de7a048c8
tatatititoto <bescond.alexandre@gmail.com>
2017-10-16 22:37:45 +0200

    mechanic part1 OK

---
+++ Assignment 2/Part1/repandre/src/model/FormatCommand.java
@@ -0,0 +1,73 @@
+package model;
+
+import application.AccountReplica;
+
+public class FormatCommand {
+	
+	private String action = null;
+	private String param1 = null;
+	private String param2 = null;
+	
+	public FormatCommand(String[] request)
+	{
+		this.action = request[0];
+		
+		if (request.length == 2)
+		{
+			this.param1 = request[1];
+		}
+		else if (request.length == 3)
+		{
+			this.param1 = request[1];
+			this.param2 = request[2];
+		}
+	}
+	
+	public String getAction() {
+		return this.action;
+	}
+
+	public String getParam1() {
+		return this.param1;
+	}
+
+	public String getParam2() {
+		return this.param2;
+	}
+
+	public void executeOn(Object object) throws UnknowAction {
+		switch (this.getAction())
+		{
+			case "exit":
+				break;
+				
+			case "balance":
+				break;
+				
+			case "deposit":
+				break;
+				
+			case "addinterest":
+				break;
+				
+			case "exchange":
+				break;
+				
+			case "memberinfo":
+				break;
+				
+			case "sleep":
+				break;
+			default:
+				throw new UnknowAction(this.action);
+		}
+		
+	}
+
+
+	
+
+	
+	
+
+}
---
+++ Assignment 2/Part1/repandre/src/model/UnknowAction.java
@@ -0,0 +1,10 @@
+package model;
+
+@SuppressWarnings("serial")
+public class UnknowAction extends Exception {
+
+	public UnknowAction(String action) {
+		super(action);
+	}
+
+}
