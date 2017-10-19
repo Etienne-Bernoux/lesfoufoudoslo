6ec3ccab6b6c731935d0d8f5cf05fe4b25853989
EtienneB <etienne.bernoux@edu.ece.fr>
2017-10-19 17:55:35 +0200

    sh file

--- Assignment 2/Delivery/Part 1/GenerateJar.ps1
+++ Assignment 2/Delivery/Part 1/GenerateJar.ps1
@@ -2,12 +2,20 @@
 New-Item ./BESCOND-BERNOUX_code-java/bin -type directory
 
 
-
-javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\spread\*
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\UnknownAction.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\UnknownCurrency.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\ErrorCurrency.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\State.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\Currency.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\Cash.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\model\ByzantineError.java
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\spread\* -nowarn 
+javac -d ./BESCOND-BERNOUX_code-java\bin\ -classpath .\BESCOND-BERNOUX_code-java\bin .\BESCOND-BERNOUX_code-java\src\application\Replica.java .\BESCOND-BERNOUX_code-java\src\model\FormatCommand.java .\BESCOND-BERNOUX_code-java\src\tools\IOFileParsing.java
 
 
 # Shame...
-cd ./BESCOND-BERNOUX_code-java\bin\
-jar cvfe ../../AccountReplica.jar application.AccountReplica ./*
+#cd ./BESCOND-BERNOUX_code-java\bin\
+#jar cvfe ../../AccountReplica.jar application.AccountReplica ./*
+#cd ..\..\
 
 
