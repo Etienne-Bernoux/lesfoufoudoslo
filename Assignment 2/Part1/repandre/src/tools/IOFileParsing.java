0f740f071da3e3614a182a883c3d0225d27fcc55
EtienneB <etienne.bernoux@edu.ece.fr>
2017-10-19 17:54:38 +0200

    small modifiac-ion on tools

--- Assignment 2/Part1/repandre/src/tools/IOFileParsing.java
+++ Assignment 2/Part1/repandre/src/tools/IOFileParsing.java
@@ -1,26 +1,20 @@
 package tools;
 
-
-
 import model.FormatCommand;
-import org.jetbrains.annotations.NotNull;
 
 /**
  * This class provide all method for parsing the input file, and command line
- *
  */
-public class  IOFileParsing {
-	
+public class IOFileParsing {
 
-	public static String[] parseLineTab(String line)
-	{
-		return line.split("\t| ");
-	}
 
-	
-	public static FormatCommand getFormatCommandFromLine(String line)
-	{
-		
-		return new FormatCommand(IOFileParsing.parseLineTab(line));
-	}
+    public static String[] parseLineTab(String line) {
+        return line.split("\t| ");
+    }
+
+
+    public static FormatCommand getFormatCommandFromLine(String line) {
+
+        return new FormatCommand(IOFileParsing.parseLineTab(line));
+    }
 }
